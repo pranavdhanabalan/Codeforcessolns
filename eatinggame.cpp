020aad0442dcca7145a5e7af95d82238f3e525da
Pranav D <dhanabalanpranav@gmail.com>
2026-03-04 17:15:36 +0530

    Refactor eatinggame.cpp to simplify logic
    Removed unordered_map and unused variables, simplified logic.

--- eatinggame.cpp
+++ eatinggame.cpp
@@ -1,28 +1,26 @@
 #include<iostream>
-#include <unordered_map>
 using namespace std;
+
 int main(){
-    unordered_map<int,int> order;
     int t,n;
+
     cin>>t;
-    for (int i=1;i<=t;i++){
+
+    while(t--){
         cin>>n;
-        int sum=0;
-        int j=1;
-        int a;
-        for (j=1;j<=n;j++){
+
+        int sum=0,a;
+
+        for(int i=0;i<n;i++){
             cin>>a;
             sum+=a;
         }
-        order[j]=sum;
+
+        if(sum % n == 0)
+            cout<<n<<endl;
+        else
+            cout<<sum % n<<endl;
     }
-    for (auto it: order){
-        if ((it.second%it.first)==0){
-            cout<<it.first<<endl;
-        }
-        else{
-            cout<<(it.second%it.first)<<endl;
-        }
-    }
+
     return 0;
 }
