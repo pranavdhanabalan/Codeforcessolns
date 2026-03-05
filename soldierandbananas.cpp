c881d35306cd98feb3e30641994202cd13c82521
Pranav D <dhanabalanpranav@gmail.com>
2026-03-05 13:49:55 +0530

    Add files via upload

---
+++ soldierandbananas.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int k,n,w,sum=0;
+    cin>>k>>n>>w;
+    for (int i=1;i<=w;i++){
+        sum+=(k*i);
+    }
+    if (sum<=n){
+        cout<<0<<endl;
+        return 0;
+    }
+    cout<<(sum-n)<<endl;
+    return 0;
+}
